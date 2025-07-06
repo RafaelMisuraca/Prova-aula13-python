c0294816e08e416543129f2ea48b9fa3d03aaedf
RafaelMisuraca <rafaelmisuraca@gmail.com>
2025-07-06 16:26:56 -0300

    Update prova13.py

--- prova13.py
+++ prova13.py
@@ -1,21 +1,21 @@
 class ContaBancaria:
-    def __init__(self, _titular, _saldo):
-        self.titular = _titular
-        self.saldo = _saldo
+    def __init__(self, titular, saldo):
+        self._titular = titular
+        self._saldo = saldo
 
     def depositar(self, valor):
-        self.saldo += valor
+        self._saldo += valor
         print(f"Depósito de {valor} realizado com sucesso.")
 
     def sacar(self, valor):
-        if valor > self.saldo:
+        if valor > self._saldo:
             print("Saldo insuficiente.")
         else:
-            self.saldo -= valor
+            self._saldo -= valor
             print(f"Saque de {valor} realizado com sucesso.")
 
     def consultar_saldo(self):
-        print(f"Saldo atual: {self.saldo}")
+        print(f"Saldo atual: {self._saldo}")
 
 
 conta = ContaBancaria("João", 1000)
